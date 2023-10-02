dcc2822421cf1adbf01a42f1f722f0b06f04cf51
Rui Simão <rui.daniel.simao@gmail.com>
2023-10-02 17:18:27 +0100

    chore(components): add tests

--- packages/components/src/Popover/__tests__/Popover.test.tsx
+++ packages/components/src/Popover/__tests__/Popover.test.tsx
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import { createRef } from 'react';
 import { testA11y } from '@interlay/test-utils';
+import userEvent from '@testing-library/user-event';
 
 import { Popover, PopoverBody, PopoverContent, PopoverFooter, PopoverHeader, PopoverTrigger } from '..';
+import { CTA } from '../../CTA';
 
 describe('Popover', () => {
   it('should render correctly', () => {
@@ -54,5 +56,119 @@ describe('Popover', () => {
         </PopoverContent>
       </Popover>
     );
+  });
+
+  it('should able to open by using trigger', async () => {
+    render(
+      <Popover>
+        <PopoverTrigger>
+          <CTA>trigger</CTA>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverHeader>header</PopoverHeader>
+          <PopoverBody>body</PopoverBody>
+          <PopoverFooter>footer</PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    );
+
+    userEvent.click(screen.getByRole('button', { name: /trigger/i }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('dialog', { name: /header/i }));
+    });
+  });
+
+  it('should able to close popover by using ESC', async () => {
+    render(
+      <Popover>
+        <PopoverTrigger>
+          <CTA>trigger</CTA>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverHeader>header</PopoverHeader>
+          <PopoverBody>body</PopoverBody>
+          <PopoverFooter>footer</PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    );
+
+    userEvent.click(screen.getByRole('button', { name: /trigger/i }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('dialog', { name: /header/i }));
+    });
+
+    userEvent.keyboard('{Escape}');
+
+    await waitForElementToBeRemoved(screen.getByRole('dialog', { name: /header/i }));
+  });
+
+  it('should able to close popover by clicking outside of the component', async () => {
+    render(
+      <Popover>
+        <PopoverTrigger>
+          <CTA>trigger</CTA>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverHeader>header</PopoverHeader>
+          <PopoverBody>body</PopoverBody>
+          <PopoverFooter>footer</PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    );
+
+    userEvent.click(screen.getByRole('button', { name: /trigger/i }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('dialog', { name: /header/i }));
+    });
+
+    userEvent.click(document.body);
+
+    await waitForElementToBeRemoved(screen.getByRole('dialog', { name: /header/i }));
+  });
+
+  it('should emit onOpenChange', async () => {
+    const handleOpenChange = jest.fn();
+
+    render(
+      <Popover onOpenChange={handleOpenChange}>
+        <PopoverTrigger>
+          <CTA>trigger</CTA>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverHeader>header</PopoverHeader>
+          <PopoverBody>body</PopoverBody>
+          <PopoverFooter>footer</PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    );
+
+    userEvent.click(screen.getByRole('button', { name: /trigger/i }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('dialog', { name: /header/i }));
+    });
+
+    expect(handleOpenChange).toHaveBeenCalledTimes(1);
+    expect(handleOpenChange).toHaveBeenCalledWith(true);
+  });
+
+  it('should be default open', async () => {
+    render(
+      <Popover defaultOpen>
+        <PopoverTrigger>
+          <CTA>trigger</CTA>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverHeader>header</PopoverHeader>
+          <PopoverBody>body</PopoverBody>
+          <PopoverFooter>footer</PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    );
+
+    expect(screen.getByRole('dialog', { name: /header/i }));
   });
 });
