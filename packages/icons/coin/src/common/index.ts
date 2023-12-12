9abb0482aa628549bd7b51153587255bceed6c51
Daniel Simão <rui.daniel.simao@gmail.com>
2023-12-12 14:57:01 +0000

    feat(icons): add common icons and change ETH icon (#45)

--- packages/icons/coin/src/common/index.ts
+++ packages/icons/coin/src/common/index.ts
@@ -2,3 +2,5 @@ export { BTC } from './BTC';
 export { ETH } from './ETH';
 export { USDT } from './USDT';
 export { ZBTC } from './ZBTC';
+export { WBTC } from './WBTC';
+export { USDC } from './USDC';
---
+++ packages/icons/common/src/CheckFillCircle.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from 'react';
+
+import { Icon, IconProps } from './core';
+
+const CheckFillCircle = forwardRef<SVGSVGElement, IconProps>((props, ref) => (
+  <Icon
+    {...props}
+    ref={ref}
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='1.5'
+    viewBox='0 0 24 24'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </Icon>
+));
+
+CheckFillCircle.displayName = 'CheckFillCircle';
+
+export { CheckFillCircle };
